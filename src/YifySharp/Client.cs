238433f65bacf769801a80df5052be573ca14a39
Ioannis G. <jtgftw@gmail.com>
2015-02-05 21:35:54 +0200

    basic client, response

---
+++ src/YifySharp/Client.cs
@@ -0,0 +1,19 @@
+﻿using RestSharp;
+
+namespace YifySharp
+{
+    public class YifyClient
+    {
+        private readonly string urlFormat = "https://yts.re/api/v2/{0}.json";
+
+        private YifyResponse<T> Execute<T>(RestRequest request, string requestEndpoint)
+        {
+            var baseUrl = string.Format(urlFormat, requestEndpoint);
+            var client = new RestClient(baseUrl);
+
+            var response = client.Execute<YifyResponse<T>>(request);
+
+            return response.Data;
+        }
+    }
+}
---
+++ src/YifySharp/Response.cs
@@ -0,0 +1,11 @@
+﻿namespace YifySharp
+{
+    public class YifyResponse<T>
+    {
+        public string Status { get; set; }
+
+        public string StatusMessage { get; set; }
+
+        public T Data { get; set; }
+    }
+}
