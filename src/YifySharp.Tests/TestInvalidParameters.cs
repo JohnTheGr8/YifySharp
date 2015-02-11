ff25ce62f077a6e86f637e0628fa22461bad519d
Ioannis G. <jtgftw@gmail.com>
2015-02-11 03:46:01 +0200

    test invalid parameters

---
+++ src/YifySharp.Tests/TestInvalidParameters.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace YifySharp.Tests
+{
+    public class TestInvalidParameters
+    {
+        YifyClient cl = new YifyClient();
+
+        [TestCase]
+        [ExpectedException(typeof(ApplicationException))]
+        public void TestInvalidParametersMovieDetails()
+        {
+            cl.GetMovieDetails(0);
+        }
+
+        [TestCase]
+        [ExpectedException(typeof(ApplicationException))]
+        public void TestInvalidParametersMovieSuggestions()
+        {
+            cl.GetMovieSuggestions(0);
+        }
+
+        [TestCase]
+        [ExpectedException(typeof(ApplicationException))]
+        public void TestInvalidParametersMovieComments()
+        {
+            cl.GetMovieComments(0);
+        }
+
+        [TestCase]
+        [ExpectedException(typeof(ApplicationException))]
+        public void TestInvalidParametersUserDetails()
+        {
+            cl.GetUserDetails(0);
+        }
+    }
+}
